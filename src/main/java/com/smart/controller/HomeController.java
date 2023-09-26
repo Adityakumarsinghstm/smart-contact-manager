6d1abf21ef893560e79bba22721e68aa141fb19c
Aditya <adityasingh62065@gmail.com>
2023-09-26 08:10:17 +0530

    lecture 44 , user registration process with message complete

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -3,12 +3,16 @@ package com.smart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
 
-
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -16,23 +20,54 @@ public class HomeController {
     private UserRepository userRepository;
 
     @RequestMapping("/")
-    public String home(Model model)
-    {
+    public String home(Model model) {
         model.addAttribute("title", "Home - smart contact manager");
         return "home";
     }
 
-
     @RequestMapping("/about")
-    public String about()
-    {
+    public String about() {
         return "about";
     }
 
     @RequestMapping("/signup")
-    public String signUp(Model model)
-    {
+    public String signUp(Model model) {
         model.addAttribute("title", "Register - smart contact manager");
+        model.addAttribute("user", new User());
         return "signup";
+    }
+
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("user") User user,
+            @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+            HttpSession session) {
+
+        try {
+            if (!agreement) {
+                System.out.println("You have not checked terms and condition ");
+                throw new Exception("You have not checked terms and condition ");
+            }
+            user.setRole("ROLE_USER");
+            user.setEnabled(true);
+            user.setImageUrl("default.png");
+
+            System.out.println("agreement   " + agreement);
+            System.out.println("User    " + user);
+
+            User result = userRepository.save(user);
+            model.addAttribute("user", result);
+
+            session.setAttribute("message", new Message("User Added successfully !!", "alert-success"));
+            return "signup";
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message("Something went wrong !!" + e.getMessage(), "alert-danger"));
+            return "signup";
+        }
+
     }
 }
