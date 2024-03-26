fa0c0369518b7c88758a011ea54a0f2fb2776905
Aditya <adityasingh62065@gmail.com>
2024-03-26 11:24:08 +0530

    lec 48 completed, implemented websecurityconfigureradapter and BcryptPasswordEncoder

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,6 +22,10 @@ import jakarta.validation.Valid;
 public class HomeController {
     @Autowired
     private UserRepository userRepository;
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping("/")
     public String home(Model model) {
@@ -60,6 +66,7 @@ public class HomeController {
             user.setRole("ROLE_USER");
             user.setEnabled(true);
             user.setImageUrl("default.png");
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
 
             System.out.println("agreement   " + agreement);
             System.out.println("User    " + user);
